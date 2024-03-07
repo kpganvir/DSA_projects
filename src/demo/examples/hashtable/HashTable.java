531a94c50f82da6137b0ef300676923588edb96c
ganvirkalpana <kalpana.ganvir@technoxander.com>
2024-03-07 14:31:47 +0530

    hashtable class updates

--- src/demo/examples/hashtable/HashTable.java
+++ src/demo/examples/hashtable/HashTable.java
@@ -1,5 +1,9 @@
 package demo.examples.hashtable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HashTable {
 
 	private int size=7;
@@ -11,13 +15,97 @@ public class HashTable {
 		dataMap=new Node[size];
 	}
 	
+	//hash method
+	
+	private int hash(String key)
+	{
+		int hash=0;
+		
+		char[] keyChars=key.toCharArray();
+		for(char ch:keyChars)
+		{
+			int aciivalue=ch;
+			//reminder after dividing by zero
+			hash=(hash+aciivalue*23) % dataMap.length;
+			
+		}
+		
+		
+		return hash;
+	}
+	
+	public void set(String key,int value)
+	{
+		
+		int index=hash(key);
+		Node newNode=new Node(key,value);
+		if(dataMap[index]==null)
+		{
+			dataMap[index]=newNode;
+			
+		}else {
+			Node temp=dataMap[index];	
+			while(temp.next!=null)
+			{
+				temp=temp.next;
+			}
+			temp.next=newNode;
+		}
+		
+	}
+	
+	public int get(String key)
+	{
+		
+		int index=hash(key);
+		Node temp=dataMap[index];
+		while(temp!=null)
+		{
+			if(temp.key==key)
+			{
+				return temp.value;
+			}
+			temp=temp.next;
+		}
+		
+		
+		return 0;
+		
+	}
+	
+	
 	void printTable()
 	{
-		for(Node node: dataMap)
+		for(int i=0;i<dataMap.length;i++)
 		{
-			System.out.println("key= "+node.getKey() +" : value = "+node.getValue());
+			System.out.println(i+":");
+			Node temp=dataMap[i];
+			while(temp!=null)
+			{
+			System.out.println("  { key= "+temp.getKey() +" : value = "+temp.getValue()+" }");
+			temp=temp.next;
+			}
 		}
 	}
+	
+	public List<String> keys()
+	{
+		List<String> keys=new ArrayList<String>();
+		for(Node temp:dataMap)
+		{
+			while(temp!=null)
+			{
+				keys.add(temp.getKey());
+				temp=temp.next;
+			}
+			
+			
+		}
+		return keys;
+		
+	}
+	
+	
 	
 	class Node{
 		
--- src/demo/examples/hashtable/HashTableDemo.java
+++ src/demo/examples/hashtable/HashTableDemo.java
@@ -6,8 +6,19 @@ public class HashTableDemo {
 		// TODO Auto-generated method stub
 
 		HashTable myHashtable=new HashTable();
+
+		myHashtable.set("paint",12);
+		myHashtable.set("bolts",100);
+		myHashtable.set("nails",4);
+		myHashtable.set("tile",6);
+		myHashtable.set("lumber",11);;
 		myHashtable.printTable();
+		System.out.println(" hi ="+myHashtable.get("hi"));
 		
+		System.out.println(" tile ="+myHashtable.get("tile"));
+		System.out.println(" lumber ="+myHashtable.get("lumber"));
+		
+		System.out.println("all keys ="+myHashtable.keys());
 	}
 
 }
